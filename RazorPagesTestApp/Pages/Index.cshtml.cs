f10b55636e734704fd8284415aca82e67f72e1a7
DmytroProg <dmytroprog@gmail.com>
2024-11-22 18:17:11 +0200

    changes

--- RazorPagesTestApp/Pages/Index.cshtml.cs
+++ RazorPagesTestApp/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesTestApp.Data;
 using RazorPagesTestApp.Models;
 
 namespace RazorPagesTestApp.Pages;
@@ -7,35 +9,41 @@ namespace RazorPagesTestApp.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly RazorPagesTestAppContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, RazorPagesTestAppContext context)
     {
         _logger = logger;
-        Students = new List<Student>()
-        {
-            new Student()
-            {
-                Id = 1,
-                FirstName = "John",
-                LastName = "Johnson",
-                LastLoggedIn = DateTime.Now,
-                PresenceType = PresenceStatus.Present,
-                IsOnline = true,
-                Grade = 12,
-                DiamondsCount = 3,
-                Comment = null
-            }
-        };
+        _context = context;
+        Students = new List<Student>();
     }
 
-    public IEnumerable<Student> Students { get; set; }
+    [BindProperty] 
+    public List<Student> Students { get; set; } = new();
 
-    public void OnGet()
+    public async Task<IActionResult> OnGetAsync()
     {
-    }
-
-    public IActionResult OnPostAddTitle()
-    {
+        Students = await _context.Student.ToListAsync();
         return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        foreach (var student in Students)
+        {
+            var studentFromDb = await _context.Student.FindAsync(student.Id);
+            studentFromDb.PresenceType = student.PresenceType;
+            studentFromDb.IsOnline = student.IsOnline;
+            studentFromDb.Grade = student.Grade;
+            studentFromDb.DiamondsCount = student.DiamondsCount;
+        }
+        
+        await _context.SaveChangesAsync();
+        return RedirectToPage("Index");
+    }
+
+    public IActionResult OnGetAddStudent()
+    {
+        return RedirectToPage("AddStudent");
     }
 }
